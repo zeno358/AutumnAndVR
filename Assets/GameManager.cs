bb290b7ef2687a7dc19516edc16b55fc859679a8
m.roppy <roppy@hotmail.co.jp>
2016-09-29 04:22:24 +0900

    ゲームマネージャとリフトのスクリプトを追加

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ゲーム全体の変数を管理
+/// </summary>
+public class GameManager : MonoBehaviour {
+	
+	/// <summary>
+	/// プレイヤー情報
+	/// </summary>
+	struct Player{
+		int count; // 踏んだ回数
+	};
+
+	Player player1;
+	Player player2;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/RIft.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+/// <summary>
+/// 上昇する物体
+/// </summary>
+public class Rift : MonoBehaviour {
+
+	float height; //高度
+
+	const float ascend_value = 0.5f; // 一回の上昇で 何M 上昇するか
+	const int ascend_cost = 5; // 何ポイントで１上昇するか？
+	int energy = 0; // 集まったエネルギー
+
+	void Start()
+	{
+		height = transform.position.y;
+		Debug.Log("スタート時点での高度は " + height.ToString() );
+	}
+
+	/// <summary>
+	/// 上昇する
+	/// </summary>
+	void Ascend()
+	{
+		height += ascend_value;
+		transform.DOMoveY( height, 0.5f);
+
+		Debug.Log(height.ToString() + "まで上昇");
+	}
+
+	/// <summary>
+	/// エネルギーを加算
+	/// </summary>
+	public void AddEnergy()
+	{
+		energy++;
+		if( energy >= ascend_cost ){
+			Ascend();
+			energy -= ascend_cost;
+		}
+	}
+}
