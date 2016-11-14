8c556b457f667749c206cefd68e8299615b2ea25
A11DC133 <roppy@hotmail.co.jp>
2016-11-15 05:38:17 +0900

    Viveコンのトリガーでもゲーム開始

--- Assets/Script/TitleScene.cs
+++ Assets/Script/TitleScene.cs
@@ -4,6 +4,9 @@ using System.Collections;
 
 public class TitleScene : MonoBehaviour {
 
+
+	SteamVR_TrackedObject trackedObject;
+
 	void Start()
 	{
 		var m = GameObject.Find("Muscle").GetComponent<Muscle>();
@@ -11,19 +14,49 @@ public class TitleScene : MonoBehaviour {
 		{
 			m.SetToOrigin();
 		}
+
+		if( UnityEditor.PlayerSettings.virtualRealitySupported )
+		{
+			StartCoroutine (TryGetVrController ());
+		}
+	}
+
+	IEnumerator TryGetVrController(){
+		do {
+			trackedObject = GameObject.Find("Controller (right)"). GetComponent<SteamVR_TrackedObject>();
+			yield return new WaitForSeconds(5);
+		} while(trackedObject == null);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if(Input.anyKeyDown)
 		{
-			var g = GameObject.Find("MatchMaker").GetComponent<AutumnVRGameManager>();
-			if(g != null)
-			{
-				g.ShowGameStartExpression();
-			}
-
-			SceneManager.UnloadScene("Title");
+			GoToMainGame ();
 		}
+
+		if (trackedObject == null) {
+			return;
+		}
+
+        var device = SteamVR_Controller.Input((int) trackedObject.index);
+
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			GoToMainGame ();
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			GoToMainGame ();
+        }
+
+	}
+
+	private void GoToMainGame(){
+		var g = GameObject.Find("MatchMaker").GetComponent<AutumnVRGameManager>();
+		if(g != null)
+		{
+			g.ShowGameStartExpression();
+		}
+
+		SceneManager.UnloadScene("Title");
 	}
 }
